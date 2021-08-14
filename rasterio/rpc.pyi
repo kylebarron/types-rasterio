16544fc3a7dc420e7c25a12282ca734009899a05
Kyle Barron <kbarron@foursquare.com>
2021-08-13 22:32:14 -0600

    Update RPC

--- rasterio/rpc.pyi
+++ rasterio/rpc.pyi
@@ -1,25 +1,30 @@
-from collections import OrderedDict as OrderedDict
-from collections.abc import Iterable as Iterable
-from typing import Any
+from typing import Dict, Sequence
+
 
 class RPC:
-    height_off: Any
-    height_scale: Any
-    lat_off: Any
-    lat_scale: Any
-    line_den_coeff: Any
-    line_num_coeff: Any
-    line_off: Any
-    line_scale: Any
-    long_off: Any
-    long_scale: Any
-    samp_den_coeff: Any
-    samp_num_coeff: Any
-    samp_off: Any
-    samp_scale: Any
-    err_bias: Any
-    err_rand: Any
-    def to_dict(self): ...
-    def to_gdal(self): ...
+    height_off: float
+    height_scale: float
+    lat_off: float
+    lat_scale: float
+    line_den_coeff: Sequence[float]
+    line_num_coeff: Sequence[float]
+    line_off: float
+    line_scale: float
+    long_off: float
+    long_scale: float
+    samp_den_coeff: Sequence[float]
+    samp_num_coeff: Sequence[float]
+    samp_off: float
+    samp_scale: float
+    err_bias: float
+    err_rand: float
+
+    def to_dict(self) -> Dict:
+        ...
+
+    def to_gdal(self) -> Dict[str, str]:
+        ...
+
     @classmethod
-    def from_gdal(cls, rpcs): ...
+    def from_gdal(cls, rpcs: Dict[str, str]):
+        ...
