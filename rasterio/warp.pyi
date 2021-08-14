4034ee862243d02ec5a2e4e604a79650993e58d4
Kyle Barron <kbarron@foursquare.com>
2021-08-13 22:20:06 -0600

    Update vrt and warp

--- rasterio/warp.pyi
+++ rasterio/warp.pyi
@@ -1,15 +1,103 @@
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+
+from affine import Affine
+from numpy.typing import ArrayLike
+
+from rasterio.control import GroundControlPoint
+from rasterio.crs import CRS
 from rasterio.enums import Resampling as Resampling
-from rasterio.env import GDALVersion as GDALVersion, ensure_env as ensure_env, require_gdal_version as require_gdal_version
-from rasterio.errors import GDALBehaviorChangeException as GDALBehaviorChangeException, TransformError as TransformError
-from rasterio.transform import array_bounds as array_bounds
-from typing import Any
+from rasterio.rpc import RPC
 
 SUPPORTED_RESAMPLING: Any
 GDAL2_RESAMPLING: Any
 
-def transform(src_crs, dst_crs, xs, ys, zs: Any | None = ...): ...
-def transform_geom(src_crs, dst_crs, geom, antimeridian_cutting: bool = ..., antimeridian_offset: float = ..., precision: int = ...): ...
-def transform_bounds(src_crs, dst_crs, left, bottom, right, top, densify_pts: int = ...): ...
-def reproject(source, destination: Any | None = ..., src_transform: Any | None = ..., gcps: Any | None = ..., rpcs: Any | None = ..., src_crs: Any | None = ..., src_nodata: Any | None = ..., dst_transform: Any | None = ..., dst_crs: Any | None = ..., dst_nodata: Any | None = ..., dst_resolution: Any | None = ..., src_alpha: int = ..., dst_alpha: int = ..., resampling=..., num_threads: int = ..., init_dest_nodata: bool = ..., warp_mem_limit: int = ..., **kwargs): ...
-def aligned_target(transform, width, height, resolution): ...
-def calculate_default_transform(src_crs, dst_crs, width, height, left: Any | None = ..., bottom: Any | None = ..., right: Any | None = ..., top: Any | None = ..., gcps: Any | None = ..., rpcs: Any | None = ..., resolution: Any | None = ..., dst_width: Any | None = ..., dst_height: Any | None = ..., **kwargs): ...
+NumType = Union[int, float]
+CRSOrDict = Union[CRS, Dict[str, str]]
+Resolution = Union[Tuple[NumType, NumType], NumType]
+Gcps = Sequence[GroundControlPoint]
+Rpcs = Union[RPC, Dict]
+
+def transform(
+    src_crs: CRSOrDict,
+    dst_crs: CRSOrDict,
+    xs: ArrayLike,
+    ys: ArrayLike,
+    zs: Optional[ArrayLike] = ...,
+) -> Tuple[ArrayLike, ...]:
+    ...
+
+
+def transform_geom(
+    src_crs: CRSOrDict,
+    dst_crs: CRSOrDict,
+    geom: Dict,
+    antimeridian_cutting: bool = ...,
+    antimeridian_offset: float = ...,
+    precision: float = ...,
+) -> Dict:
+    ...
+
+
+def transform_bounds(
+    src_crs: CRSOrDict,
+    dst_crs: CRSOrDict,
+    left: float,
+    bottom: float,
+    right: float,
+    top: float,
+    densify_pts: int = ...,
+) -> Tuple[float, float, float, float]:
+    ...
+
+
+# TODO: should also allow a rasterio.Band object in addition to ArrayLike for the source
+# and destination
+def reproject(
+    source: ArrayLike,
+    destination: Optional[ArrayLike] = ...,
+    src_transform: Optional[Affine] = ...,
+    gcps: Optional[Gcps] = ...,
+    rpcs: Optional[Rpcs] = ...,
+    src_crs: Optional[CRSOrDict] = ...,
+    src_nodata: Optional[NumType] = ...,
+    dst_transform: Optional[Affine] = ...,
+    dst_crs: Optional[CRSOrDict] = ...,
+    dst_nodata: Optional[NumType] = ...,
+    dst_resolution: Optional[Resolution] = ...,
+    src_alpha: int = ...,
+    dst_alpha: int = ...,
+    resampling: Resampling=...,
+    num_threads: int = ...,
+    init_dest_nodata: bool = ...,
+    warp_mem_limit: int = ...,
+    **kwargs
+) -> Tuple[ArrayLike, Affine]:
+    ...
+
+
+def aligned_target(
+    transform: Affine,
+    width: int,
+    height: int,
+    resolution: Resolution,
+) -> Tuple[Affine, int, int]:
+    ...
+
+
+def calculate_default_transform(
+    src_crs: CRSOrDict,
+    dst_crs: CRSOrDict,
+    width: int,
+    height: int,
+    left: Optional[float] = ...,
+    bottom: Optional[float] = ...,
+    right: Optional[float] = ...,
+    top: Optional[float] = ...,
+    gcps: Optional[Gcps] = ...,
+    rpcs: Optional[Rpcs] = ...,
+    resolution: Resolution = ...,
+    dst_width: Optional[int] = ...,
+    dst_height: Optional[int] = ...,
+    **kwargs: Any
+) -> Tuple[Affine, int, int]:
+    ...
