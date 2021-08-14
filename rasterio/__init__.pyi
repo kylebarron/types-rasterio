33b380fc4e6e9ab8f1517342a7cc8dc080b9c3f0
Kyle Barron <kbarron@foursquare.com>
2021-08-13 23:34:09 -0600

    Update init

--- rasterio/__init__.pyi
+++ rasterio/__init__.pyi
@@ -1,13 +1,50 @@
-from rasterio.env import Env as Env
-from typing import Any, NamedTuple
+from typing import Any, Callable, NamedTuple, Optional, Sequence, Tuple, TypeVar, Union
 
-def open(fp, mode: str = ..., driver: Any | None = ..., width: Any | None = ..., height: Any | None = ..., count: Any | None = ..., crs: Any | None = ..., transform: Any | None = ..., dtype: Any | None = ..., nodata: Any | None = ..., sharing: bool = ..., **kwargs): ...
+from affine import Affine
+from numpy.typing import NBitBase, NDArray
+
+from rasterio.io import BufferedDatasetWriter, DatasetReader, DatasetWriter, MemoryFile
+
+Dataset = Union[DatasetReader, DatasetWriter, BufferedDatasetWriter, MemoryFile]
+Bands = Union[int, Sequence[int]]
+
+
+def open(
+    fp,
+    mode: str = ...,
+    driver: Any | None = ...,
+    width: Any | None = ...,
+    height: Any | None = ...,
+    count: Any | None = ...,
+    crs: Any | None = ...,
+    transform: Any | None = ...,
+    dtype: Any | None = ...,
+    nodata: Any | None = ...,
+    sharing: bool = ...,
+    **kwargs
+):
+    ...
+
 
 class Band(NamedTuple):
-    ds: Any
-    bidx: Any
-    dtype: Any
-    shape: Any
+    ds: Dataset
+    bidx: Bands
+    dtype: str
+    shape: Tuple[int, ...]
 
-def band(ds, bidx): ...
-def pad(array, transform, pad_width, mode: Any | None = ..., **kwargs): ...
+
+def band(ds: Dataset, bidx: Bands) -> Band:
+    ...
+
+
+T = TypeVar("T", bound=NBitBase)
+
+
+def pad(
+    array: NDArray[T],
+    transform: Affine,
+    pad_width: int,
+    mode: Optional[Union[str, Callable]] = ...,
+    **kwargs: Any
+) -> Tuple[NDArray[T], Affine]:
+    ...
