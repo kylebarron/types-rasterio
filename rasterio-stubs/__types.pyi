9171900d3f06931726c81219b271c12e8c76a9bc
Kyle Barron <kbarron@foursquare.com>
2021-08-17 19:04:34 -0600

    Update IO typing

--- rasterio-stubs/__types.pyi
+++ rasterio-stubs/__types.pyi
@@ -1,14 +1,29 @@
-from typing import Dict, Sequence, Tuple, TypeVar, Union
+from types import TracebackType
+from typing import BinaryIO, Dict, Optional, Sequence, Tuple, Type, TypeVar, Union
 
 from numpy.typing import NBitBase
 from rasterio.crs import CRS
 from rasterio.windows import Window
 
-NDArrayGenericT = TypeVar("T", bound=NBitBase)
+# Generic types
+NDArrayGenericT = TypeVar("NDArrayGenericT", bound=NBitBase)
 
+# Alias types
 Colormap = Dict[int, Union[Tuple[int, int, int], Tuple[int, int, int, int]]]
+Count = int
 CRSInput = Union[str, Dict[str, str], CRS]
+Driver = str
+FileOrBytes = Union[BinaryIO, bytes]
+Height = int
 Indexes = Union[int, Sequence[int]]
 Nodata = float
 NumType = Union[int, float]
+Width = int
 WindowInput = Union[Window, Tuple[Tuple[int, int], Tuple[int, int]]]
+
+# Context manager types
+# https://mypy.readthedocs.io/en/stable/protocols.html#contextmanager-t
+ExcType = Optional[Type[BaseException]]
+ExcValue = Optional[BaseException]
+Traceback = Optional[TracebackType]
+ExitReturn = Optional[bool]
