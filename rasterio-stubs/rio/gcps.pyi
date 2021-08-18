279443432dfde3f37d688b2c9a9ddb2f9070000e
Kyle Barron <kylebarron2@gmail.com>
2021-08-17 19:56:44 -0600

    Remove CLI stubs (#7)

--- rasterio-stubs/rio/gcps.pyi
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-from rasterio.rio import options as options
-from rasterio.warp import transform_geom as transform_geom
-
-sequence_opt: Any
-
-def gcps(ctx, input, geojson_type, projection, precision, use_rs, indent, compact): ...
--- rasterio-stubs/rio/info.pyi
+++
@@ -1,6 +0,0 @@
-from rasterio.rio import options as options
-from rasterio.transform import from_gcps as from_gcps
-
-def info(
-    ctx, input, aspect, indent, namespace, meta_member, verbose, bidx, masked
-) -> None: ...
