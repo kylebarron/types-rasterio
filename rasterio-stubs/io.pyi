c2e9cf3379e43c61baa3e80019207f7e8a1fbe41
Kyle Barron <kylebarron2@gmail.com>
2021-08-17 19:07:28 -0600

    Update IO types (#5)

--- rasterio-stubs/io.pyi
+++ rasterio-stubs/io.pyi
@@ -1,56 +1,80 @@
-from typing import Any, BinaryIO, Optional, Union
+from typing import Any, Optional, Type, TypeVar, Union
 
-from rasterio._base import driver_can_create, driver_can_create_copy, get_dataset_driver
+from affine import Affine
+from numpy.typing import DTypeLike
+from rasterio.__types import (
+    Count,
+    CRSInput,
+    Driver,
+    ExcType,
+    ExcValue,
+    ExitReturn,
+    FileOrBytes,
+    Height,
+    Nodata,
+    Traceback,
+    Width,
+)
 from rasterio._io import (
     BufferedDatasetWriterBase,
     DatasetReaderBase,
     DatasetWriterBase,
     MemoryFileBase,
 )
-from rasterio.env import ensure_env, env_ctx_if_needed
-from rasterio.path import UnparsedPath
 from rasterio.transform import TransformMethodsMixin
 from rasterio.windows import WindowMethodsMixin
 
-log: Any
+class DatasetReader(DatasetReaderBase, WindowMethodsMixin, TransformMethodsMixin):
+    def __repr__(self) -> str: ...
 
-FileOrBytes = Optional[Union[BinaryIO, bytes]]
+class DatasetWriter(DatasetWriterBase, WindowMethodsMixin, TransformMethodsMixin):
+    def __repr__(self) -> str: ...
 
-class DatasetReader(DatasetReaderBase, WindowMethodsMixin, TransformMethodsMixin): ...
-class DatasetWriter(DatasetWriterBase, WindowMethodsMixin, TransformMethodsMixin): ...
 class BufferedDatasetWriter(
     BufferedDatasetWriterBase, WindowMethodsMixin, TransformMethodsMixin
-): ...
+):
+    def __repr__(self) -> str: ...
+
+MemoryFileSelf = TypeVar("MemoryFileSelf", bound="MemoryFile")
 
 class MemoryFile(MemoryFileBase):
     def __init__(
-        self,
-        file_or_bytes: FileOrBytes = ...,
+        self: MemoryFileSelf,
+        file_or_bytes: Optional[FileOrBytes] = ...,
         dirname: Optional[str] = ...,
         filename: Optional[str] = ...,
         ext: str = ...,
     ) -> None: ...
     def open(
-        self,
-        driver: Any | None = ...,
-        width: Any | None = ...,
-        height: Any | None = ...,
-        count: Any | None = ...,
-        crs: Any | None = ...,
-        transform: Any | None = ...,
-        dtype: Any | None = ...,
-        nodata: Any | None = ...,
+        self: MemoryFileSelf,
+        driver: Driver | None = ...,
+        width: Width | None = ...,
+        height: Height | None = ...,
+        count: Count | None = ...,
+        crs: CRSInput | None = ...,
+        transform: Affine | None = ...,
+        dtype: DTypeLike | None = ...,
+        nodata: Nodata | None = ...,
         sharing: bool = ...,
-        **kwargs
+        **kwargs: Any
     ) -> Union[DatasetReader, DatasetWriter]: ...
-    def __enter__(self): ...
-    def __exit__(self, *args, **kwargs) -> None: ...
+    def __enter__(self: MemoryFileSelf) -> MemoryFileSelf: ...
+    def __exit__(
+        self: MemoryFileSelf,
+        exc_type: ExcType,
+        exc_value: ExcValue,
+        traceback: Traceback,
+    ) -> ExitReturn: ...
 
 class ZipMemoryFile(MemoryFile):
     def __init__(self, file_or_bytes: FileOrBytes = ...) -> None: ...
     def open(
-        self, path, driver: Any | None = ..., sharing: bool = ..., **kwargs: Any
-    ): ...
+        self, path: str, driver: Driver | None = ..., sharing: bool = ..., **kwargs: Any
+    ) -> DatasetReader: ...
 
-def get_writer_for_driver(driver): ...
-def get_writer_for_path(path, driver: Any | None = ...): ...
+def get_writer_for_driver(
+    driver: Driver,
+) -> Optional[Union[Type[DatasetWriter], Type[BufferedDatasetWriter]]]: ...
+def get_writer_for_path(
+    path: str, driver: Driver | None = ...
+) -> Optional[Union[Type[DatasetWriter], Type[BufferedDatasetWriter]]]: ...
