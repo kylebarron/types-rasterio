ff0fc305237eb0eab8d8b9779fe46c10732dbff0
Kyle Barron <kbarron@foursquare.com>
2021-08-17 18:40:19 -0600

    Add _warp

--- rasterio-stubs/__types.pyi
+++ rasterio-stubs/__types.pyi
@@ -1,9 +1,14 @@
-from typing import Dict, Tuple, Union
+from typing import Dict, Tuple, Union, Sequence, TypeVar
+from numpy.typing import NBitBase
 
 from rasterio.crs import CRS
 from rasterio.windows import Window
+
+NDArrayGenericT = TypeVar("T", bound=NBitBase)
 
 WindowInput = Union[Window, Tuple[Tuple[int, int], Tuple[int, int]]]
 Colormap = Dict[int, Union[Tuple[int, int, int], Tuple[int, int, int, int]]]
 CRSInput = Union[str, Dict[str, str], CRS]
 NumType = Union[int, float]
+Nodata = float
+Indexes = Union[int, Sequence[int]]
